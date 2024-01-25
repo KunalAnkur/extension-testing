68b8bb74bd7b9c3fd948689344eae66d4bad9867
Ankur Kunal <57923976+KunalAnkur@users.noreply.github.com>
2024-01-25 22:41:05 +0530

    second commit

--- background.js
+++ background.js
@@ -1,15 +1,8 @@
 // background.js
 chrome.webRequest.onCompleted.addListener(
-    function(details) {
-      console.log("Request completed:", details);
-      
+    function (details) {
+      console.log("Request completed: ", details);
       // You can access various details of the request in the 'details' object
-      // For example, details.url, details.method, details.statusCode, etc.
-      
-      // Perform actions based on the completed request if needed
-      
     },
-    { urls: ["<all_urls>"] }, // You can specify the URL patterns you want to monitor
-    ["responseHeaders"]
+    { urls: ["<all_urls>"] } // Specify the URL patterns you want to monitor
   );
-  
